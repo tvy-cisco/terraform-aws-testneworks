325fb05ed01f9d99ffb80a2d1cda6b40908d9ecd
tvy <tvy@cisco.com>
2025-05-21 13:01:33 -0600

    adds my public key

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    profile = "strln"
-    region  = "us-west-2"
+  profile = "strln"
+  region  = "us-west-2"
 }
 
 ###################
@@ -32,7 +32,7 @@ resource "aws_internet_gateway" "main-igw" {
 resource "aws_subnet" "public_subnet_5" {
   vpc_id                          = aws_vpc.terraform_vpc.id
   cidr_block                      = "10.0.1.0/24"
-  ipv6_cidr_block                = cidrsubnet(aws_vpc.terraform_vpc.ipv6_cidr_block, 8, 0)
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.terraform_vpc.ipv6_cidr_block, 8, 0)
   assign_ipv6_address_on_creation = true
   map_public_ip_on_launch         = true
   availability_zone               = "us-west-2a"
@@ -43,12 +43,12 @@ resource "aws_subnet" "public_subnet_5" {
 }
 
 resource "aws_subnet" "private_subnet_5" {
-  vpc_id                          = aws_vpc.terraform_vpc.id
-  ipv6_cidr_block                = cidrsubnet(aws_vpc.terraform_vpc.ipv6_cidr_block, 8, 1)
-  assign_ipv6_address_on_creation = true
-  ipv6_native                     = true
+  vpc_id                                         = aws_vpc.terraform_vpc.id
+  ipv6_cidr_block                                = cidrsubnet(aws_vpc.terraform_vpc.ipv6_cidr_block, 8, 1)
+  assign_ipv6_address_on_creation                = true
+  ipv6_native                                    = true
   enable_resource_name_dns_aaaa_record_on_launch = true
-  availability_zone               = "us-west-2a" 
+  availability_zone                              = "us-west-2a"
 
   tags = {
     Name = "terraform-Network5-private"
@@ -81,7 +81,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.terraform_vpc.id
 
   route {
-    ipv6_cidr_block = "::/0"
+    ipv6_cidr_block      = "::/0"
     network_interface_id = aws_instance.dns64_nat64.primary_network_interface_id
   }
 
@@ -105,16 +105,16 @@ resource "aws_route_table_association" "private" {
 ###################
 
 resource "aws_security_group" "dns64_nat64" {
-  name        = "terraform-Network5-dns64-nat64"
-  vpc_id      = aws_vpc.terraform_vpc.id
+  name   = "terraform-Network5-dns64-nat64"
+  vpc_id = aws_vpc.terraform_vpc.id
 
   # SSH access from specific IP
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["66.249.77.67/32"]  # The specific IPv4 address
-    description      = "SSH access from specific IPv4 address"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["66.249.77.67/32"] # The specific IPv4 address
+    description = "SSH access from specific IPv4 address"
   }
 
   # DNS queries
@@ -130,18 +130,18 @@ resource "aws_security_group" "dns64_nat64" {
   ingress {
     from_port        = -1
     to_port          = -1
-    protocol         = "58"  # ICMPv6 protocol number
-    ipv6_cidr_blocks = ["::/0"]  # Allow from anywhere
+    protocol         = "58"     # ICMPv6 protocol number
+    ipv6_cidr_blocks = ["::/0"] # Allow from anywhere
     description      = "ICMPv6 (ping6)"
   }
 
   # ICMPv4 (ping)
   ingress {
-    from_port        = -1
-    to_port          = -1
-    protocol         = "icmp"
-    cidr_blocks      = ["0.0.0.0/0"]  # Allow from anywhere
-    description      = "ICMP (ping)"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere
+    description = "ICMP (ping)"
   }
 
   # All IPv6 traffic from VPC
@@ -169,8 +169,8 @@ resource "aws_security_group" "dns64_nat64" {
 }
 
 resource "aws_security_group" "test_instance" {
-  name        = "terraform-Network5-test"
-  vpc_id      = aws_vpc.terraform_vpc.id
+  name   = "terraform-Network5-test"
+  vpc_id = aws_vpc.terraform_vpc.id
 
   ingress {
     from_port        = 22
@@ -199,7 +199,12 @@ resource "aws_security_group" "test_instance" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "terraform-network5-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4BHU0dKBfL6sFaFHdqHeQOrzj9cmAwWpLMAvN0DCys sshahary@cisco.com" # Replace with your actual public key
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4BHU0dKBfL6sFaFHdqHeQOrzj9cmAwWpLMAvN0DCys sshahary@cisco.com"
+}
+
+resource "aws_key_pair" "thomas_key" {
+  key_name   = "thomas_key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOGZx8IAu+Wki/2mmBDlj5ICeut+tsuPo8cu5tRC0tN4 tvy@cisco.com"
 }
 
 ###################
@@ -207,13 +212,13 @@ resource "aws_key_pair" "deployer" {
 ###################
 
 resource "aws_instance" "dns64_nat64" {
-  ami                    = "ami-0735c191cf914754d"  # Ubuntu 22.04 LTS
+  ami                    = "ami-0735c191cf914754d" # Ubuntu 22.04 LTS
   instance_type          = "t3.small"
   subnet_id              = aws_subnet.public_subnet_5.id
   ipv6_address_count     = 1
-  source_dest_check      = false  # Required for NAT functionality
+  source_dest_check      = false # Required for NAT functionality
   vpc_security_group_ids = [aws_security_group.dns64_nat64.id]
-  key_name               = aws_key_pair.deployer.key_name  # Add this line
+  key_name               = aws_key_pair.thomas_key.key_name # Add this line
 
   user_data = file("${path.module}/scripts/dns64_nat64_setup.sh")
 
@@ -223,12 +228,12 @@ resource "aws_instance" "dns64_nat64" {
 }
 
 resource "aws_instance" "test_instance" {
-  ami                    = "ami-0735c191cf914754d"  # Ubuntu 22.04 LTS
+  ami                    = "ami-0735c191cf914754d" # Ubuntu 22.04 LTS
   instance_type          = "t3.small"
   subnet_id              = aws_subnet.private_subnet_5.id
   ipv6_address_count     = 1
   vpc_security_group_ids = [aws_security_group.test_instance.id]
-  key_name               = aws_key_pair.deployer.key_name  # Add this line
+  key_name               = aws_key_pair.thomas_key.key_name # Add this line
 
   user_data = templatefile("${path.module}/scripts/test_instance_setup.sh.tpl", {
     dns64_server_ipv6 = aws_instance.dns64_nat64.ipv6_addresses[0]
@@ -252,3 +257,4 @@ output "test_instance_ipv6" {
   description = "IPv6 address of the test instance"
   value       = aws_instance.test_instance.ipv6_addresses[0]
 }
+
