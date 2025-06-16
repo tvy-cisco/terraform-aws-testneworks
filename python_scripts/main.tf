ad5456fa73bf57071c2b5254d521eae0d674a77d
tvy-cisco <tvy+sbg@cisco.com>
2025-06-16 16:57:50 -0600

    fixes filter for ec2 instances

--- python_scripts/main.tf
+++ python_scripts/main.tf
@@ -4,40 +4,44 @@ provider "aws" {
 }
 
 data "aws_instance" "mac_test_instance" {
-  tags = {
-    Name = "Mac Test Instance"
+  filter {
+    name   = "tag:Name"
+    values = ["Mac Test Instance"]
   }
 }
 output "mac_test_ip" {
   description = "Ipv6 address of mac test instance"
-  value       = data.aws_instance.mac_test_instance.ipv6_addresses[0]
+  value       = tolist(data.aws_instance.mac_test_instance.ipv6_addresses)[0]
 }
 
 data "aws_instance" "linux_test_instance" {
-  tags = {
-    Name = "Linux Test Instance"
+  filter {
+    name   = "tag:Name"
+    values = ["Linux Test Instance"]
   }
 }
 
 output "linux_test_ip" {
   description = "IPv6 address of the linux test instance"
-  value       = data.aws_instance.linux_test_instance.ipv6_addresses[0]
+  value       = tolist(data.aws_instance.linux_test_instance.ipv6_addresses)[0]
 }
 
 data "aws_instance" "windows_test_instance" {
-  tags = {
-    Name = "Windows Test Instance"
+  filter {
+    name   = "tag:Name"
+    values = ["Windows Test Instance"]
   }
 }
 
 output "windows_test_ip" {
   description = "IPv6 address of the windows test instance"
-  value       = data.aws_instance.windows_test_instance.ipv6_addresses[0]
+  value       = tolist(data.aws_instance.windows_test_instance.ipv6_addresses)[0]
 }
 
 data "aws_instance" "jumpbox" {
-  tags = {
-    Name = "Jumpbox Instance"
+  filter {
+    name   = "tag:Name"
+    values = ["Jumpbox Instance"]
   }
 }
 
@@ -47,14 +51,15 @@ output "jumpbox_ip" {
 }
 
 data "aws_instance" "n5_gateway" {
-  tags = {
-    Name = "Network 5 Gateway"
+  filter {
+    name   = "tag:Name"
+    values = ["Network 5 Gateway"]
   }
 }
 
 output "n5_gateway_ipv6" {
   description = "IPv6 address of the Network 5 gateway server"
-  value       = data.aws_instance.n5_gateway.ipv6_addresses[0]
+  value       = tolist(data.aws_instance.n5_gateway.ipv6_addresses)[0]
 }
 
 output "n5_gateway_ipv4" {
@@ -64,18 +69,19 @@ output "n5_gateway_ipv4" {
 
 output "n5_gateway_network_interface_id" {
   description = "Network interface id of n5 gateway"
-  value       = data.aws_instance.n5_gateway.primary_network_interface_id
+  value       = data.aws_instance.n5_gateway.network_interface_id
 }
 
 data "aws_instance" "n4_gateway" {
-  tags = {
-    Name = "Network 4 Gateway"
+  filter {
+    name   = "tag:Name"
+    values = ["Network 4 Gateway"]
   }
 }
 
 output "n4_gateway_ipv6" {
   description = "IPv6 address of the Network 5 gateway server"
-  value       = data.aws_instance.n4_gateway.ipv6_addresses[0]
+  value       = tolist(data.aws_instance.n4_gateway.ipv6_addresses)[0]
 }
 
 output "n4_gateway_ipv4" {
@@ -85,7 +91,7 @@ output "n4_gateway_ipv4" {
 
 output "n4_gateway_network_interface_id" {
   description = "Network interface id of n4 gateway"
-  value       = data.aws_instance.n4_gateway.primary_network_interface_id
+  value       = data.aws_instance.n4_gateway.network_interface_id
 }
 
 data "aws_route_table" "private" {
